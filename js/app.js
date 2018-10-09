8b057aa94b67660c1ed634e157f82209eeac89af
jared <jaredpattison@users.noreply.github.com>
2018-10-09 14:46:06 -0700

    Question 1 works

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
 'use strict'
 
-alert('IT IS ALIVE!!!')
+alert('Welcome! Let\'s play a game.');
+
+var username = prompt("What is your name?");
+
+alert('Welcome, ' + username + '! Let\'s get started!');
+
+var answerOne = prompt('Do I have more than three pets?');
+
+if (answerOne === 'yes') {
+    alert('Right!');
+    console.log('user got question 1 correct')
+}else{
+    alert('Wrong');
+    console.log('The user got question 1 WRONG!')
+}

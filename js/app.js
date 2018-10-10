633366c39f433005f4643725d4113db784b33534
jared <jaredpattison@users.noreply.github.com>
2018-10-10 10:54:26 -0700

    New CSS and HTML

--- js/app.js
+++ js/app.js
@@ -15,13 +15,12 @@ if (answerOne === 'yes' || answerOne === 'y') {
     alert('Wrong');
     console.log('The user got question 1 WRONG!')
 }
-
 var answerTwo = prompt('Do I like winning?').toLowerCase();
 
 if (answerTwo === 'yes' || answerTwo === 'y') {
     alert('Right! Winner winner chicken dinner!');
     console.log('user got question 2 correct')
-}else{
+} else {
     alert('Wrong, I like winning!');
     console.log('The user got question 2 WRONG!')
 }
@@ -30,7 +29,7 @@ var answerThree = prompt('Am I cool?').toLowerCase();
 if (answerThree === 'yes' || answerThree === 'y') {
     alert('Yes! At least I like to think so');
     console.log('user got question 3 right!')
-}else{
+} else {
     alert('Wrong');
     console.log('The user got question 3 WRONG!')
 }
@@ -39,7 +38,7 @@ var answerFour = prompt('Does Jared make the best hamburgers?').toLowerCase();
 if (answerFour === 'yes' || answerFour === 'y') {
     alert('Right! Of course he does!');
     console.log('user got question 4 correct')
-}else{
+} else {
     alert('Wrong, his burgers are the yummmiest!');
     console.log('The user got question 1 WRONG!')
 }
@@ -48,7 +47,7 @@ var answerFive = prompt('Are there any more questions after this one?').toLowerC
 if (answerFive === 'yes' || answerFive === 'y') {
     alert('WRONG!, we are done.');
     console.log('user got question 5 wrong!')
-}else{
+} else {
     alert('Correct and goodbye.');
     console.log('The user got question 1 correct.')
 }

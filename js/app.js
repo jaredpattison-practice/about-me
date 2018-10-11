8a8a190b78dd24ce3a6093d318feab1adb0702e1
rkgallaway <rkgallaway@msn.com>
2018-10-11 13:32:51 -0700

    Logic moved into functions

--- js/app.js
+++ js/app.js
@@ -8,92 +8,116 @@ var username = prompt("What is your name?");
 
 alert('Welcome, ' + username + '! Let\'s get started!');
 
-var answerOne = prompt('Do I have more than three pets?').toLowerCase();
+function questionOne(){
+  var answerOne = prompt('Do I have more than three pets?').toLowerCase();
 
-if (answerOne === 'yes' || answerOne === 'y') {
+  if (answerOne === 'yes' || answerOne === 'y') {
     alert('Wrong!, I am not that crazy.');
     console.log('user got question 1 correct');
-}else{
+  }else{
     alert('Right, two is alredy too many.');
     console.log('The user got question 1 correct');
     tally += 1;
+  }
 }
-var answerTwo = prompt('Do I like winning?').toLowerCase();
 
-if (answerTwo === 'yes' || answerTwo === 'y') {
+function questionTwo(){
+  var answerTwo = prompt('Do I like winning?').toLowerCase();
+
+  if (answerTwo === 'yes' || answerTwo === 'y') {
     alert('Right! Winner winner chicken dinner!');
     console.log('user got question 2 correct');
     tally += 1;
-} else {
+  } else {
     alert('Wrong, I like winning!');
     console.log('The user got question 2 WRONG!');
+  }
 }
-var answerThree = prompt('Am I cool?').toLowerCase();
 
-if (answerThree === 'yes' || answerThree === 'y') {
+function questionThree(){
+  var answerThree = prompt('Am I cool?').toLowerCase();
+
+  if (answerThree === 'yes' || answerThree === 'y') {
     alert('Yes! At least I like to think so');
     console.log('user got question 3 right!');
     tally += 1;
-} else {
+  } else {
     alert('Wrong');
     console.log('The user got question 3 WRONG!');
+  }
 }
-var answerFour = prompt('Does Jared make the best hamburgers?').toLowerCase();
 
-if (answerFour === 'yes' || answerFour === 'y') {
+function questionFour(){
+  var answerFour = prompt('Does Jared make the best hamburgers?').toLowerCase();
+
+  if (answerFour === 'yes' || answerFour === 'y') {
     alert('Right! Of course he does!');
     console.log('user got question 4 correct');
     tally += 1;
-} else {
+  } else {
     alert('Wrong, his burgers are the yummmiest!');
     console.log('The user got question 1 WRONG!');
+  }
 }
-var answerFive = prompt('Are there any more questions after this one?').toLowerCase();
 
-if (answerFive === 'yes' || answerFive === 'y') {
+function questionFive(){
+  var answerFive = prompt('Are there any more questions after this one?').toLowerCase();
+
+  if (answerFive === 'yes' || answerFive === 'y') {
     alert('Right, we are not done.');
     console.log('user got question 5 correct!');
     tally += 1;
-} else {
+  } else {
     alert('Sorry, not done yet.');
     console.log('The user got question 5 wrong.');
+  }
 }
 
-var i = 4;
-alert('Time to play a guessing game.')
-do { 
+function questionSix(){
+  var i = 4;
+  alert('Time to play a guessing game.')
+  do { 
     var answerSix = Number(prompt('Give me a number.'));
     i--;
-     if (answerSix === 17) {
-            tally++;
-            alert('Yes, that is correct.')
-            console.log('The user got question 6 correct')
-            break;
-        } if (answerSix < 17){
-            alert('Too low.')
-        } if (answerSix > 17){
-            alert('Too hight')
-        } if (i === 0) {
-            alert('Sorry ' + username + ', You are out of guesses')
-        }  
-    }
-
-
-while (i > 0);
-
-var j = 0;
-do {
-    var answerSeven = prompt('Guess one of my favorite foods.').toLowerCase();
-        if (answers.includes(answerSeven)){
-            alert('GREAT');
-            tally ++;
-            console.log('user got one more correct');
-            break;
-        } else {
-            j ++;
-            alert('wrong');
-        }
+    if (answerSix === 17) {
+      tally++;
+      alert('Yes, that is correct.')
+      console.log('The user got question 6 correct')
+      break;
+    } if (answerSix < 17){
+      alert('Too low.')
+    } if (answerSix > 17){
+      alert('Too hight')
+    } if (i === 0) {
+      alert('Sorry ' + username + ', You are out of guesses')
+    }  
+  }
+  while (i > 0);
 }
-while (j < 7);
+
+function questionSeven(){
+  var j = 0;
+  do {
+    var answerSeven = prompt('Guess one of my favorite foods.').toLowerCase();
+    if (answers.includes(answerSeven)){
+      alert('GREAT');
+      tally ++;
+      console.log('user got one more correct');
+      break;
+    } else {
+      j ++;
+      alert('wrong');
+    }
+  }
+  while (j < 7);
+}
+
+questionOne();
+questionTwo();
+questionThree();
+questionFour();
+questionFive();
+questionSix();
+questionSeven();
 
 alert('Good job ' + username + ', you got ' + tally + ' right.');
